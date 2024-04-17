cc29e31e700166b0b36aafbf40709fd744c14c90
Vitali Bibikov <vitali@bringiton.games>
2024-04-17 23:22:27 +0200

    Updated delete

--- src/TransactionalWriter.cs
+++ src/TransactionalWriter.cs
@@ -64,10 +64,40 @@ public sealed class TransactionalWriter : IAsyncDisposable
         AddRawRequest(request);
     }
 
-    public void DeleteAsync<T>(KeyValue key)
-        where T : ITransactional
+    public void DeleteAsync<T>(string key, string value)
     {
-        var request = new DeleteTransactionRequest<T>(key);
+        var request = new DeleteTransactionRequest<T>(new KeyValue()
+        {
+            Key = key,
+            Value = value
+        });
+
+        AddRawRequest(request);
+    }
+
+    public void DeleteAsync<TModel, TKeyValue>(
+        Expression<Func<TModel, string>> propertyExpression,
+        string value)
+    {
+        var member = propertyExpression.Body as MemberExpression;
+
+        if (value == null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        if (member == null)
+        {
+            throw new ArgumentException("Expression is not a member access", nameof(propertyExpression));
+        }
+        var propertyName = member.Member.Name;
+
+        var request = new DeleteTransactionRequest<TModel>(new KeyValue()
+        {
+            Key = propertyName,
+            Value = value
+        });
+
         AddRawRequest(request);
     }
 
